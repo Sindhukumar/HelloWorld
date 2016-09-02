0e6b92b02dd79d7dd396fe8a8a69e99b733b6d2c
Sindhukumar <ssundar6@gmu.edu>
2016-09-02 11:16:15 -0400

    inital commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld {
+	
+	public static void main(String[] args){
+		System.out.println("Hello World");
+		System.out.println(System.currentTimeMillis());int main;
+		main = 100;
+		System.out.println(main);
+	}
+}
